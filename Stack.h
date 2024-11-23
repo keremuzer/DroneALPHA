692bbc8cea31590a12ac6160285339315af44861
keremuzer <keremuzer33@gmail.com>
2024-11-23 16:13:09 +0300

    implement stack functionality and define maximum size

--- Stack.h
+++ Stack.h
@@ -1,14 +1,15 @@
 #ifndef STACK_H
 #define STACK_H
 
-#define MAX_SIZE 0  //TO DO: Define a maximum size for the stack to control memory usage considering the problem at hand
+#define MAX_SIZE 81 // TO DO: Define a maximum size for the stack to control memory usage considering the problem at hand
 
-class Stack {
+class Stack
+{
 public:
-    int top;                  // Index of the top element in the stack
-    int data[MAX_SIZE];       // Static array to store provinces as integers (province IDs)
+    int top;            // Index of the top element in the stack
+    int data[MAX_SIZE]; // Static array to store provinces as integers (province IDs)
 
-    Stack();                  // Constructor to initialize the stack
+    Stack(); // Constructor to initialize the stack
 
     // Adds a province to the top of the stack
     void push(int province);
@@ -23,7 +24,7 @@ public:
     bool isEmpty() const;
 
     // Returns the current size of the stack
-    int getSize() const;  
+    int getSize() const;
 };
 
 #endif // STACK_H
